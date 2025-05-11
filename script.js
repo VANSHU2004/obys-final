efe75f574d9b5580e1bc9806ce6531d005047d58
Atul Sharma <atulsharma02004@gmail.com>
2025-05-11 08:58:52 +0530

    Created Loader Successfully

---
+++ script.js
@@ -0,0 +1,49 @@
+var tl = gsap.timeline()
+
+tl.from(".line h1",{
+    y: "150%",
+    duration:0.7,
+    stagger:0.2,
+    delay:0.5
+});
+
+tl.from('#line1-part1',{
+    opacity:0,
+    onStart:function(){
+        var h5timer = document.querySelector("#line1-part1 h5");
+        var grow = 0;
+
+        let timer = setInterval(function(){
+        grow += 1;
+        h5timer.innerHTML = grow;
+        if(grow >= 100){
+        clearInterval(timer);
+        }
+
+        },35);
+    }
+});
+tl.to('.line h2',{
+    animationName:'anime',
+    opacity:1
+})
+
+tl.to('#loader',{
+    opacity:0,
+    display:'none',
+    duration:0.2,
+    delay:3.3
+}); 
+
+tl.from('#page1',{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    ease:Power4,
+    duration:0.5
+});
+
+
+
+
+
